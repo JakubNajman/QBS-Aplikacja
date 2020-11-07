d072b38f5b85312ff0fa52468c617085a51208a2
JakubNajman <najman00@outlook.com>
2020-11-07 20:53:42 +0100

    Dodaję porównywanie i wyszukiwanie sekwencji bajtów.

--- Main.java
+++ Main.java
@@ -2,43 +2,42 @@ import java.io.*;
 import java.util.*;
 import java.lang.Math;
 import javax.swing.JFrame;
-
 import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
 
-public class Main{
+public class Main {
 
-    public static byte[] byteReader(Path path) {
-        try{
+    public static List<Byte> byteReader(Path path) {
+        try {
             byte[] data = Files.readAllBytes(path);
-            return data;
-        }catch (Exception e){
+            List<Byte> endData = new ArrayList<Byte>();
+            for(int i = 0; i<data.length; i++){
+                endData.add(data[i]);
+            }
+            return endData;
+        } catch (Exception e) {
             return null;
         }
     }
 
-    public static byte[] toByteArray(String string) {
-        try{
-            byte[] converted = string.getBytes(StandardCharsets.UTF_8);
-            return converted;
-        }catch (Exception e){
+    public static Integer[] byteComparator(List<Byte> motherSequence, List<Byte> userInput) {
+        try {
+            int begin = Collections.indexOfSubList(motherSequence, userInput);
+            System.out.println(begin);
+            Integer[] borders = { begin, begin + userInput.size() };
+            return borders;
+        } catch (Exception e) {
             return null;
         }
     }
 
     public static void main(String[] args) {
-        
-        // JFrame frame = new JFrame(); 
-        // frame.setTitle("ByteChanger 2020");
-        // frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // frame.setSize(300,250);
-        // frame.setVisible(true);
-        Path path1 = Paths.get("C:/Users/najma/Desktop/");
-        System.out.println(byteReader(path1));
-        int len = byteReader(path1).length;
-        System.out.println(len+ "ddd");
-        for(int i=0;  i<=len; i++){
-            System.out.println(byteReader(path1)[i]);
+
+        Path path1 = Paths.get("C:/Users/najma/Desktop/test.txt");
+        Path path2 = Paths.get("C:/Users/najma/Desktop/test2.txt");
+        Integer[] ends = byteComparator(byteReader(path1), byteReader(path2));
+        for (int i = 0; i < ends.length; i++) {
+            System.out.println(ends[i]);
         }
     }
 }
